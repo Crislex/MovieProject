485f1acc6e89bc86d31a05bda95420921693186d
Coreas19 <66981308+Coreas19@users.noreply.github.com>
2021-03-24 00:13:23 -0600

    Update web.php

--- MovieProject-app/routes/web.php
+++ MovieProject-app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProyectoController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', ProyectoController::class);
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
